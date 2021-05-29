ff127e95677d4e20654ff5d889fd2c80a3b97bc0
Aleix Alonso <aleixalonso0@gmail.com>
2021-05-29 15:55:21 +0200

    possibility to move between two, halo when moving, light changes, slower camera effect speed

--- Assets/scripts/MoveTrash.cs
+++ Assets/scripts/MoveTrash.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class MoveTrash : MonoBehaviour
 {
     public int peopleToMove = 1;
+    //public float movementSpeed = 1.0f;
 
     private int currentPeopleMoving = 0;
     private bool isMoving = false;
-    private GameObject objectCollide = null;
+
+    private GameObject player1 = null;
+    private GameObject player2 = null;
+
+    private GameObject playerAlone = null;
+
+    private GameObject Halo = null;
+
     private float timeGrabbed = 0.0f;
     private float speed = 0.0f;
     private Vector3 oldPosition = Vector3.zero;
@@ -17,41 +25,132 @@ public class MoveTrash : MonoBehaviour
     void Start()
     {
         oldPosition = transform.position;
+        if(transform.Find("Halo")) Halo = transform.Find("Halo").gameObject;
+        if(Halo) Halo.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        float speedRB = GetComponent<Rigidbody>().velocity.magnitude;
-        if(isMoving && objectCollide && peopleToMove == currentPeopleMoving)
+        if (!isMoving)
         {
-            //speed = Vector3.Distance(oldPosition, transform.position) * 100f;
-            //oldPosition = transform.position;
-            newPosition = transform.position;
-            var media = (newPosition - oldPosition);
-            speed = (media / Time.deltaTime).magnitude;
-            oldPosition = newPosition;
-            newPosition = transform.position;
+            /*
+            UnityEngine.Random.InitState(System.DateTime.Now.Millisecond);
+            float movementSpeed = 0.05f;
+            Vector3 newPosition = new Vector3(Random.Range(9, 90), 0, Random.Range(9, 90));
+            Debug.Log(newPosition);
+            if(transform.position != newPosition)
+            {
+                transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * movementSpeed);
+            }
+            //float randomX = Random.Range(-0.5f, 0.5f);
+            //float randomZ = Random.Range(-0.5f, 0.5f);
+            //transform.position += new Vector3(randomX, 0, randomZ);
+            */
+        }
+        if(isMoving && peopleToMove == 1 && peopleToMove == currentPeopleMoving)
+        {
+            if (player1)
+            {
+                playerAlone = player1;
+            }
+            else if (player2)
+            {
+                playerAlone = player2;
+            }
 
-            //Debug.Log("RIGID BODY SPEED: " + speedRB.ToString("F2"));
+            if (playerAlone)
+            {
+                enableHalo();
+                newPosition = transform.position;
+                var media = (newPosition - oldPosition);
+                speed = (media / Time.deltaTime).magnitude;
+                oldPosition = newPosition;
+                newPosition = transform.position;
 
+                timeGrabbed += Time.deltaTime;
+                transform.SetPositionAndRotation(playerAlone.transform.position, transform.rotation);
 
-            timeGrabbed += Time.deltaTime;
-            transform.SetPositionAndRotation(objectCollide.transform.position, transform.rotation);
-
-            if (timeGrabbed > 2.0) {
-                //Debug.Log("UNITY SPEED: " + speed.ToString("F2"));
-                if (speed > 120.0)
+                if (timeGrabbed > 2.0)
                 {
-                    //Debug.Log("ENTRO PORQUE SPEED: " + speed.ToString("F2"));
-                    isMoving = false;
-                    objectCollide = null;
-                    timeGrabbed = 0.0f;
-                    currentPeopleMoving = 0;
+                    //Debug.Log("UNITY SPEED: " + speed.ToString("F2"));
+                    if (speed > 120.0)
+                    {
+                        //Debug.Log("ENTRO PORQUE SPEED: " + speed.ToString("F2"));
+                        isMoving = false;
+                        if (playerAlone.CompareTag("Player1"))
+                        {
+                            player1 = null;
+                        }
+                        else if (playerAlone.CompareTag("Player2"))
+                        {
+                            player2 = null;
+                        }
+                        playerAlone = null;
+                        timeGrabbed = 0.0f;
+                        currentPeopleMoving = 0;
+                        disableHalo();
+                    }
+
+                }
+            } 
+        }
+        if (isMoving && peopleToMove == 2)
+        {
+            if(currentPeopleMoving < peopleToMove)
+            {
+                if (player1)
+                {
+                    playerAlone = player1;
+                }
+                else if (player2)
+                {
+                    playerAlone = player2;
+                }
+                if (playerAlone)
+                {
+                    timeGrabbed += Time.deltaTime;
+                    transform.SetPositionAndRotation(playerAlone.transform.position, transform.rotation);
+
+                    if (timeGrabbed > 2.0)
+                    {
+                        isMoving = false;
+                        if (playerAlone.CompareTag("Player1"))
+                        {
+                            player1 = null;
+                        }
+                        else if (playerAlone.CompareTag("Player2"))
+                        {
+                            player2 = null;
+                        }
+                        playerAlone = null;
+                        timeGrabbed = 0.0f;
+                        currentPeopleMoving--;
+                    }
                 }
 
             }
-        }  
+            else if(player1 && player2)
+            {
+                enableHalo();
+                timeGrabbed += Time.deltaTime;
+                float dist = Vector3.Distance(player1.transform.position, player2.transform.position);
+                if(dist > 5.0f)
+                {
+                    isMoving = false;
+                    player1 = null;
+                    player2 = null;
+                    playerAlone = null;
+                    timeGrabbed = 0.0f;
+                    currentPeopleMoving=0;
+                    disableHalo();
+                }
+                else
+                {
+                    transform.SetPositionAndRotation((player1.transform.position + player2.transform.position) / 2, transform.rotation);
+                }
+            }
+        }
 
     }
 
@@ -59,9 +158,36 @@ public class MoveTrash : MonoBehaviour
     {
         if (!isMoving || (isMoving && currentPeopleMoving < peopleToMove))
         {
+            Debug.Log("ENTRO AL ON TRIGGER CON OTHER: " + other.name);
             isMoving = true;
-            objectCollide = other.gameObject;
-            currentPeopleMoving += 1;
+            if (other.CompareTag("Player1") && !player1)
+            {
+                player1 = other.gameObject;
+                currentPeopleMoving += 1;
+                Debug.Log("PONGO PLAYER 1");
+            }
+            else if(other.CompareTag("Player2") && !player2)
+            {
+                player2 = other.gameObject;
+                currentPeopleMoving += 1;
+                Debug.Log("PONGO PLAYER 2");
+            }
+            
+        }
+    }
+
+    private void enableHalo()
+    {
+        if (Halo)
+        {
+            Halo.SetActive(true);
+        }
+    }
+    private void disableHalo()
+    {
+        if (Halo)
+        {
+            Halo.SetActive(false);
         }
     }
 }
